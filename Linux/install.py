7687bbfe659b4392b7d79a31134bd4fa995bc402
初心不忘 <897081475@qq.com>
2025-07-01 18:46:12 +0800

    3

--- Linux/install.py
+++ Linux/install.py
@@ -7,14 +7,11 @@ import sys
 import re
 import stat
 import subprocess 
-from proxy import stop_proxy
-
 
 # Prevent running as root on Linux
 if platform.system() == 'Linux' and os.environ['USER'] == 'root':
     print("This addon cannot be installed as root!", file=sys.stderr)
     sys.exit(1)
-
 
 # Check whether Python 3 is in PATH
 def checkpy():
@@ -43,14 +40,10 @@ def checkpy():
 if platform.system() == 'Windows':
     checkpy()
 
-
 # File & directory paths
-# 检测操作系统并设置相应的插件路径
 if getattr(sys, 'frozen', False):
-    # 如果是打包后的exe文件
     PKG_PATH = os.path.dirname(sys.executable)
 else:
-    # 如果是普通Python脚本
     PKG_PATH = os.path.dirname(os.path.abspath(__file__))
 
 APPNAME = 'wps-zotero_1.0.0'
@@ -61,11 +54,9 @@ elif platform.system() == 'Linux':  # Linux
 else:  # Windows
     ADDON_PATH = os.path.join(os.environ['APPDATA'], 'kingsoft', 'wps', 'jsaddons')
 
-# 定义 XML 文件路径
 XML_PATHS = {
     'publish': os.path.join(ADDON_PATH, 'publish.xml'),
 }
-PROXY_PATH = ADDON_PATH + os.path.sep + 'proxy.py'
 
 if platform.system() == 'Linux':
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -74,7 +65,6 @@ if platform.system() == 'Linux':
         file_path = os.path.join(script_dir, file_name)
         if os.path.isfile(file_path):
             try:
-                # 添加执行权限
                 current_mode = os.stat(file_path).st_mode
                 os.chmod(file_path, 0o755)
                 print(f"Added execute permission to {file_name}")
@@ -84,9 +74,6 @@ if platform.system() == 'Linux':
             print(f"Warning: {file_name} not found in {script_dir}")
 
 def uninstall():
-    print("Trying to quit proxy server if it's currently listening...")
-    stop_proxy()
-    
     def del_rw(action, name, exc):
         os.chmod(name, stat.S_IWRITE)
         os.remove(name)
@@ -110,17 +97,14 @@ def uninstall():
             with open(fp, 'w') as f:
                 f.write(xmlStr[:r[0]] + xmlStr[r[1]:])
 
-
 # Uninstall existing installation
 print('Uninstalling previous installations if there is any ...')
 uninstall()
 if len(sys.argv) > 1 and sys.argv[1] == '-u':
     sys.exit()
 
-
 # Begin installation
 print('Installing')
-
 
 # Create necessary directory and files
 if not os.path.exists(ADDON_PATH):
@@ -132,11 +116,8 @@ if not os.path.exists(XML_PATHS['publish']):
 </jsplugins>
 ''')
 
-
-
 # Copy to jsaddons
 shutil.copytree(PKG_PATH, ADDON_PATH + os.path.sep + APPNAME)
-
 
 # Write records to XML files
 def register(fp, tagname, record):
@@ -150,13 +131,10 @@ def register(fp, tagname, record):
     with open(fp, 'w') as f:
         f.write(content[:i] + record + os.linesep + content[i:])
 
-
 rec = '<jsplugin url="http://127.0.0.1:3889/" type="wps" enable="enable_dev" install="null" version="1.0.0" name="wps-zotero"/>'
 register(XML_PATHS['publish'], 'jsplugins', rec)
 
-
-# Alleviate the "Zotero window not brought to front" problem.
-# https://www.zotero.org/support/kb/addcitationdialog_raised
+# Alleviate the "Zotero window not brought to front" problem
 if os.name == 'nt':
     print('Change zotero preference to alleviate the problem of Zotero window not showing in front.')
     tmp = os.environ['APPDATA'] + '\\Zotero\\Zotero\\Profiles\\'
@@ -172,32 +150,7 @@ if os.name == 'nt':
             with open(pref_fn, 'w') as f:
                 f.write(content)
 
-
-
 print('All done, enjoy!')
 print('(run ./install.py -u to uninstall  or  python3 install.py -u to uninstall)\n卸载请执行 run ./install.py -u to uninstall  或  python3 install.py -u to uninstall')
-# 在macOS上，安装完成后打开Zotero和WPS
-if platform.system() == 'Darwin':  # macOS
-    try:
-        
-        # 授权proxy
-        app_path = '~/Library/Containers/com.kingsoft.wpsoffice.mac/Data/.kingsoft/wps/jsaddons/wps-zotero_1.0.0/proxy.app'
 
-        # 使用 os.path.expanduser 展开 '~' 到完整路径
-        expanded_app_path = os.path.expanduser(app_path)
-
-        # 使用 subprocess.run 打开应用程序
-        subprocess.run(['open', expanded_app_path])
-
-        # 打开Zotero
-        subprocess.run(['open', '-a', 'Zotero'])
-        
-
-        # 打开WPS
-        subprocess.run(['open', '-a', 'wpsoffice'])
-        
-
-
-        
-    except Exception as e:
-        print(f"无法打开应用程序: {e}")
+# macOS post-install steps
