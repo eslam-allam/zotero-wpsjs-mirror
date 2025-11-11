60880c0e8fff741ceaa669c5b30ce45476569128
初心不忘 <897081475@qq.com>
2025-11-11 19:26:35 +0800

    mac新增卸载方式

--- Win/js/about.js
+++ Win/js/about.js
@@ -1,59 +1,63 @@
-async function onbuttonclick(idStr)
-{
+async function onbuttonclick(idStr) {
     const osInfo = detectOS();
-    switch(idStr)
-    {
-        
-        case "updateZotero":{
+    switch (idStr) {
+
+        case "updateZotero": {
 
             wps.TabPages.OpenWebUrl("https://gitee.com/wangrui5015/Zotero-WPSJS");
-            
-                break;
-            }
-            case "uninstallZotero":{
-                const conValue=window.Application.confirm("您即将卸载此插件！是否继续？")
-            
-                if (!conValue) {
-                    return; // 用户取消时直接退出
-                }
-                if(conValue){
 
-                    if(osInfo=="windows"){
-                        window.Application.ActiveDocument.Save()
-                  
-                       
-                        window.Application.ActiveDocument.Saved= true
-                        await  postRequestXHRAsync('http://127.0.0.1:21931/stopproxy', null)
-                        
-                        runServers("uninstallZoteroAddin",wps.Env.GetAppDataPath() + '/kingsoft/wps/jsaddons/winUninstall.exe','jsMaximizedFocus');
-                         const tmpJson=window.Application.Env.GetTempPath()+"/settings.json"
-                         if(window.Application.FileSystem.Exists(tmpJson)){
-                            window.Application.FileSystem.Remove(tmpJson)
-                         }
-                        window.Application.Quit(-1)
-                       
-                        alert("卸载成功！")
-                        return;
-                    }else if(osInfo=="macos"){
-                        window.Application.ActiveDocument.Save()
-                      
-                        await  postRequestXHRAsync('http://127.0.0.1:21931/stopproxy', null)
-                        runServers("uninstallZoteroAdd",'open '+ wps.Env.GetHomePath() + '/.kingsoft/wps/jsaddons/macUninstall.app','jsMaximizedFocus');
-                        const tmpJson=window.Application.Env.GetTempPath()+"/settings.json"
-                        if(window.Application.FileSystem.Exists(tmpJson)){
-                           window.Application.FileSystem.Remove(tmpJson)
-                        }
-                        window.Application.Quit(0)
-                     
-                        alert("卸载成功！")
-                        return;
+            break;
+        }
+        case "uninstallZotero": {
+            const conValue = window.Application.confirm("您即将卸载此插件！是否继续？")
+
+            if (!conValue) {
+                return; // 用户取消时直接退出
+            }
+            if (conValue) {
+
+                if (osInfo == "windows") {
+                    window.Application.ActiveDocument.Save()
+
+
+                    window.Application.ActiveDocument.Saved = true
+                    await postRequestXHRAsync('http://127.0.0.1:21931/stopproxy', null)
+
+                    runServers("uninstallZoteroAddin", wps.Env.GetAppDataPath() + '/kingsoft/wps/jsaddons/winUninstall.exe', 'jsMaximizedFocus');
+                    const tmpJson = window.Application.Env.GetTempPath() + "/settings.json"
+                    if (window.Application.FileSystem.Exists(tmpJson)) {
+                        window.Application.FileSystem.Remove(tmpJson)
+                    }
+                    window.Application.Quit(-1)
+
+                    alert("卸载成功！")
+                    return;
+                } else if (osInfo == "macos") {
+                    window.Application.ActiveDocument.Save()
+
+                    await postRequestXHRAsync('http://127.0.0.1:21931/stopproxy', null)
+                    const uninstallPath = wps.Env.GetHomePath() + '/.kingsoft/wps/jsaddons/macUninstall.app';
+                    if (window.Application.FileSystem.Exists(uninstallPath)) {
+                        runServers("uninstallZoteroAdd", 'open ' + uninstallPath, 'jsMaximizedFocus');
+                    } else {
+                        runServers("uninstallZoteroAdd", 'rm -r ' + wps.Env.GetHomePath() + '/.kingsoft/wps/jsaddons/wps-zotero_1.0.0', 'jsMaximizedFocus');
                     }
 
+                    const tmpJson = window.Application.Env.GetTempPath() + "/settings.json"
+                    if (window.Application.FileSystem.Exists(tmpJson)) {
+                        window.Application.FileSystem.Remove(tmpJson)
+                    }
+                    window.Application.Quit(0)
+
+                    alert("卸载成功！")
+                    return;
                 }
-           
-                }
-                default:
-   
+
+            }
+
+        }
+        default:
+
     }
     return true;
 }
